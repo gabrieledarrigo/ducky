deab8c6e3e13194a370a8fb51c1473ac9532d58b
Gabriele D'Arrigo <darrigo.g@gmail.com>
2020-07-18 12:45:36 +0200

    Refactored tests for response_to_string function. Added a \n charachter to each response

--- src/response.c
+++ src/response.c
@@ -6,7 +6,7 @@
 char *response_to_string(response res) {
     // Allocate the len of data + 3 for the status code + 1 for the null terminator
     char *buffer = malloc(strlen(res.data) + 4);
-    sprintf(buffer, "%i %s", res.status_code, res.data);
+    sprintf(buffer, "%i %s\n", res.status_code, res.data);
     buffer[strlen(buffer)] = '\0';
 
     return buffer;
--- tests/response_test.c
+++ tests/response_test.c
@@ -1,12 +1,37 @@
 #include <stdio.h>
+#include "../lib/greatest.h"
 #include "../src/response.h"
 
-int main() {
-    response res;
-    res.status_code = SUCCESS;
-    res.data = "Lorem ipsum ipsum ipsum";
+TEST should_convert_a_response_into_a_string(void) {
+    char buffer[128];
+    response responses[] = {
+        { 200, "Lorem ipsum ipsum ipsum" },
+        { 201, "STATUS_CREATED" },
+        { 500 ,"ERR_UNKNOWN"},
+        { 501 ,"ERR_CANNOT_RECV"},
+        { 502 ,"ERR_COMMAND_NOT_RECOGNIZED"},
+        { 503 ,"ERR_MAX_DATA_SIZE"},
+        { 504 ,"ERR_KEY_LENGTH"},
+        { 505 ,"ERR_NO_KEY"},
+        { 505 ,"ERR_NO_DATA"},
+    };
 
-    char* str = response_to_string(res);
+    for (int i = 0; i < sizeof(responses) / sizeof(responses[0]); i++) {
+        sprintf(buffer, "%i %s\n", responses[i].status_code, responses[i].data);
+        ASSERT_STR_EQ(buffer, response_to_string(responses[i]));
+    }
 
-    printf("%s", str);
+    PASS();
+}
+
+SUITE(suite) {
+    RUN_TEST(should_convert_a_response_into_a_string);
+}
+
+GREATEST_MAIN_DEFS();
+
+int main(int argc, char *argv[]) {
+    GREATEST_MAIN_BEGIN();
+    RUN_SUITE(suite);
+    GREATEST_MAIN_END();
 }
